28d233b5b20ac2ebfe941a35860a7cbe044e61e1
jxie <506512796@qq.com>
2018-07-11 16:17:38 +0800

    enhance

--- public/js/app.js
+++ public/js/app.js
@@ -50380,15 +50380,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 /* harmony default export */ __webpack_exports__["default"] = ({
     mounted: function mounted() {
-        // window.postMessage(JSON.stringify({"userVerify": "check_access"}));
+        window.postMessage(JSON.stringify({ "userVerify": "check_access" }));
         this.userVerify();
     },
 
     methods: {
         userVerify: function userVerify(authKey) {
-            if (!authKey) {
-                authKey = 'MS92L0lwL3Z6VHVXSHZsTGprNGNsUT09';
-            }
             var stat = location.search.substr(1);
             window.location.href = '/clients/oauth2/redirect?' + stat + '&stat=' + authKey;
         }
