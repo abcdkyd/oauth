b4c09183595912770f2920b5dd131e2fdaddc887
jxie <506512796@qq.com>
2018-07-11 16:15:29 +0800

    enhance

--- resources/assets/js/components/PrepareAuthorize.vue
+++ resources/assets/js/components/PrepareAuthorize.vue
@@ -4,14 +4,11 @@
 <script>
     export default {
         mounted() {
-            // window.postMessage(JSON.stringify({"userVerify": "check_access"}));
+            window.postMessage(JSON.stringify({"userVerify": "check_access"}));
             this.userVerify();
         },
         methods: {
             userVerify(authKey) {
-                if (!authKey) {
-                    authKey = 'MS92L0lwL3Z6VHVXSHZsTGprNGNsUT09';
-                }
                 let stat = location.search.substr(1);
                 window.location.href = '/clients/oauth2/redirect?' + stat + '&stat=' + authKey;
             }
